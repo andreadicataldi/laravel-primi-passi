663cda4c54e3fe83113be1ca1418c2c0cbdeecb7
Andrea Di Cataldi <andrea.dicataldi@gmail.com>
2021-02-10 15:49:35 +0100

    created base html and link between two pages

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@include('layouts.head')
+<body>
+<h2>About</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus animi asperiores ex inventore laborum quaerat repellat totam! Accusantium aliquid cum, eligendi iste maiores molestiae neque non quis, repudiandae temporibus unde?</p>
+<a href="/">Return</a>
+</body>
+</html>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<header>
+    <h1>Test Layout Laravel</h1>
+</header>
+<main>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto, debitis deserunt enim est fugiat nemo neque non odio perspiciatis porro qui rem, sit totam vero voluptate voluptatum. Ducimus, odio.
+    </p>
+</main>
+<footer>
+    <p>
+        Copyright Reserved
+    </p>
+    <a href="about">About</a>
+</footer>
+</body>
+</html>
